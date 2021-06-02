39f74974d535cc4205a71b9f21e01637fd51fff1
Nevno <1002366723@qq.com>
2021-06-02 18:54:20 +0800

    update some information

--- 算法/全排列/Apple.java
+++ 算法/全排列/Apple.java
@@ -9,7 +9,7 @@ public class Apple {
 
         Scanner in = new Scanner(System.in);
 
-        while (in.hasNextInt()){
+        while (in.hasNextInt()) {
             int m = in.nextInt();
             int n = in.nextInt();
 
@@ -20,15 +20,14 @@ public class Apple {
 
     // 苹果相同，盘子相同
     // 递归条件，苹果和盘子的数量不能为0
-    public static int count(int m, int n){
+    public static int count(int m, int n) {
         // 只有一个苹果，返回1
-        if (m < 0 || n <0)
+        if (m < 0 || n < 0)
             return 0;
-        else if (m == 1 || n==1){
+        else if (m == 1 || n == 1) {
             return 1;
-        }
-        else{
-            return  count(m, n - 1) + count(m - n, n);
+        } else {
+            return count(m, n - 1) + count(m - n, n);
         }
     }
 }
