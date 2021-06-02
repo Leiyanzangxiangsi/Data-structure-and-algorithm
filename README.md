e75cd1666a086f27f48078f97dfc3b2c2ea852c2
Nevno <1002366723@qq.com>
2021-06-02 17:15:32 +0800

    update some information

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Data-structure-and-algorithm
 自己整理得一些典型数据结构和算法题
+包括各个公司的一些真题
--- 数据测试用/test.java
+++ 数据测试用/test.java
@@ -1,8 +1,10 @@
 
 // 本题为考试单行多行输入输出规范示例，无需提交，不计分。
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
 public class test {
     public static void main(String[] args) {
 //        Scanner in = new Scanner(System.in);
@@ -11,16 +13,16 @@ public class test {
 //        for (int i=0;i<n;i++)
 //            nums[i] = in.nextInt();
 
-        int[] nums = {1,2,3,4,5,6};
+        int[] nums = {1, 2, 3, 4, 5, 6};
 
         List<Integer> l = new ArrayList<>();
-        for (int a:nums)
+        for (int a : nums)
             l.add(a);
 
         l.remove(3);
         System.out.println(l.toString());
 
-        for (int i=0;i<l.size();i=i+2)
+        for (int i = 0; i < l.size(); i = i + 2)
             l.remove(i);
         System.out.println(l.toString());
     }
